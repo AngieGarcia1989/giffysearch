02a65e092b25dca098015d35418247ea24e7f7fe
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-01 19:37:56 -0500

    Creando nuevo useState para loading/ Creating new useState for loading

--- src/Components/ListOfGifs.js
+++ src/Components/ListOfGifs.js
@@ -2,21 +2,25 @@ import React, {useState, useEffect} from 'react'
 import Gif from './Gif'
 import Getgif from '../Service/Getgif';
 
+
+
 export default function ListOfGifs ({params}) {
   const {keyword}= params
+  const [loading,setloading]= useState([false])
   const [gifs,setGifs] = useState([])
 
   useEffect(function () {
+    setloading(true)
     Getgif({keyword}).then(change => setGifs(change))
-
+    setloading(false)
   },[keyword])
 
-
+if (loading) return<img className='carga' src="../Imagens/tenor.png" alt="cargando..."/>
 
     return <div>
     { 
     gifs.map(({id,title,url,images}) => 
-    
+  
         <Gif
           Key={id}
           title={title} 
