6b03c46d9e00701d3c096fc14e57010a9f1cbf81
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-06 19:07:00 -0500

    Creando estado para pasar keyword a input usando useLocation

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import {Link} from 'wouter'
+import React,{useState} from 'react'
+import {Link,useLocation} from 'wouter'
 
 const POPULAR_GIFS = ["matrix","chile", "colombia", "ecuador"]
 
 export default function Home() {
+    const [keyword,setkeyword] = useState ("")
+    const [path,pushLocation] = useLocation()
+
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        //navegar a otra ruta
+        pushLocation(`/search/${keyword}`)
+    }
+
+    const handlechange = evt => {
+        setkeyword(evt.target.value)
+    }
     return(
         <>
+        <form onSubmit={handleSubmit}>
+       
+        <input placeholder="search a gif here..." onChange={handlechange} type="text" value={keyword}/>
+            
+        </form>
         <h3 className="App-title">Los Giffs más populares </h3>
+        
         <ul>
 { POPULAR_GIFS.map((popularGif) =>(
     <li key={popularGif}>
